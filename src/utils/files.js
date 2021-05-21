d59600f11a2daf2122b5bd77a77a224ac7c4bbf8
Dan Schultz <slifty@gmail.com>
2021-05-20 20:56:49 -0400

    Add file and payload utils
    These utilities will be used later as we are parsing payloads and
    writing content to SRT files.
    
    Issue #1

---
+++ src/utils/files.js
@@ -0,0 +1,19 @@
+import fs from 'fs'
+import dayjs from 'dayjs'
+
+export function loadAndParseJsonFile(path) {
+	const rawData = fs.readFileSync(path)
+	const data = JSON.parse(rawData)
+	return data
+}
+
+export function isEmptyFile(path) {
+	const stats = fs.statSync(path)
+	return stats.size === 0
+}
+
+export function generateSegmentFileName(segmentPayload) {
+	const segmentStartTime = dayjs(segmentPayload.origin).add(segmentPayload.position)
+	const segmentData = JSON.parse(segmentPayload.data.toString('utf8'))
+	return `${segmentData.title}_${segmentStartTime.format('YY-MM-DD-HH-mm-ss')}.txt`
+}
